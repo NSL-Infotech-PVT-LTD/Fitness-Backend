eb0fcf8c310f7f1841747e36374b2b5dd6460911
Gaurav Sethi <gaurav@netscapelabs.com>
2019-08-02 12:01:07 +0530

    changes by jatinder for freelancer register and update

--- app/Http/Controllers/API/RegisterController.php
+++ app/Http/Controllers/API/RegisterController.php
@@ -11,7 +11,7 @@ use DB;
 class RegisterController extends ApiController {
 
     public function frelancerRegister(Request $request) {
-        $rules = ['firstname' => 'required', 'phone' => '', 'email' => 'required|email|unique:users', 'profile_pic' => 'required', 'bio' => 'required', 'category_id' => 'required', 'experience' => 'required', 'hourly_rate' => 'required', 'latitude' => '', 'longitude' => '', 'portfolio_image_1' => 'required', 'portfolio_image_2' => '', 'portfolio_image_3' => '', 'portfolio_image_4' => ''];
+        $rules = ['firstname' => 'required', 'phone' => '', 'email' => 'required|email|unique:users', 'profile_pic' => 'required', 'bio' => 'required', 'category_id' => 'required', 'experience' => 'required', 'hourly_rate' => 'required', 'latitude' => '', 'longitude' => '', 'portfolio_image_1' => '', 'portfolio_image_2' => '', 'portfolio_image_3' => '', 'portfolio_image_4' => ''];
         $validateAttributes = parent::validateAttributes($request, 'POST', array_merge($this->requiredParams, $rules), array_keys($rules), false);
         if ($validateAttributes):
             return $validateAttributes;
@@ -26,9 +26,13 @@ class RegisterController extends ApiController {
             $portfolioimageName = [];
             for ($i = 1; $i <= 4; $i++):
                 $portfolio = 'portfolio_image_' . $i;
-                $portfolioimageName[] = parent::__uploadImage($request->$portfolio, public_path('uploads/freelancer/portfolio'));
+                if (isset($request->$portfolio))
+                    $portfolioimageName[] = parent::__uploadImage($request->$portfolio, public_path('uploads/freelancer/portfolio'));
             endfor;
-            $input['portfolio_image'] = json_encode($portfolioimageName);
+            if (count($portfolioimageName) > 0)
+                $input['portfolio_image'] = json_encode($portfolioimageName);
+            else
+                $input['portfolio_image'] = null;
             $input['category_id'] = $request->category_id;
             $user = \App\User::create($input);
             $user->assignRole(\App\Role::where('id', 2)->first()->name);
@@ -46,7 +50,7 @@ class RegisterController extends ApiController {
             return parent::error('User Not found');
         if ($user->hasRole('freelancer') === false)
             return parent::error('Please use valid token');
-        $rules = ['firstname' => 'required', 'phone' => '', 'email' => 'required|email|unique:users,email,' . \Auth::id(), 'profile_pic' => 'required', 'bio' => 'required', 'category_id' => 'required', 'experience' => 'required', 'hourly_rate' => 'required', 'latitude' => '', 'longitude' => '', 'portfolio_image_1' => 'required', 'portfolio_image_2' => '', 'portfolio_image_3' => '', 'portfolio_image_4' => ''];
+        $rules = ['firstname' => 'required', 'phone' => '', 'email' => 'required|email|unique:users,email,' . \Auth::id(), 'profile_pic' => 'required', 'bio' => 'required', 'category_id' => 'required', 'experience' => 'required', 'hourly_rate' => 'required', 'latitude' => '', 'longitude' => '', 'portfolio_image_1' => '', 'portfolio_image_2' => '', 'portfolio_image_3' => '', 'portfolio_image_4' => ''];
         $validateAttributes = parent::validateAttributes($request, 'POST', array_merge($this->requiredParams, $rules), array_keys($rules), false);
         if ($validateAttributes):
             return $validateAttributes;
@@ -58,12 +62,16 @@ class RegisterController extends ApiController {
             endif;
             $input = $request->all();
             $input['profile_pic'] = parent::__uploadImage($request->profile_pic, public_path('uploads/freelancer/profile_pic'));
-            $portfolioimageName = [];
+             $portfolioimageName = [];
             for ($i = 1; $i <= 4; $i++):
                 $portfolio = 'portfolio_image_' . $i;
-                $portfolioimageName[] = parent::__uploadImage($request->$portfolio, public_path('uploads/freelancer/portfolio'));
+                if (isset($request->$portfolio))
+                    $portfolioimageName[] = parent::__uploadImage($request->$portfolio, public_path('uploads/freelancer/portfolio'));
             endfor;
-            $input['portfolio_image'] = json_encode($portfolioimageName);
+            if (count($portfolioimageName) > 0)
+                $input['portfolio_image'] = json_encode($portfolioimageName);
+//            else
+//                $input['portfolio_image'] = null;
             $input['category_id'] = $request->category_id;
             $user->fill($input);
             $user->save();
