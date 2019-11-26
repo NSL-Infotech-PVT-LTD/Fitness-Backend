f8fcb81a1db41405aca15e37d67f2dfb23be180a
niti prabha <niti.prabha@netscapelabs.com>
2019-11-26 16:42:48 +0530

    message to push

--- app/Http/Controllers/API/ConfigurationController.php
+++ app/Http/Controllers/API/ConfigurationController.php
@@ -30,6 +30,63 @@ class ConfigurationController extends ApiController {
         }
     }
     
+     public function getaboutus(Request $request) {
+
+
+        $rules = [];
+        $validateAttributes = parent::validateAttributes($request, 'GET', $rules, array_keys($rules), false);
+        if ($validateAttributes):
+            return $validateAttributes;
+        endif;
+        // dd($category_id);
+        try {
+            $model = new MyModel();
+            $model = $model->select('about_us');
+            
+            return parent::success($model->get());
+
+        } catch (\Exception $ex) {
+            return parent::error($ex->getMessage());
+        }
+    }
+     public function getaboutus(Request $request) {
+
+
+        $rules = [];
+        $validateAttributes = parent::validateAttributes($request, 'GET', $rules, array_keys($rules), false);
+        if ($validateAttributes):
+            return $validateAttributes;
+        endif;
+        // dd($category_id);
+        try {
+            $model = new MyModel();
+            $model = $model->select('about_us');
+            
+            return parent::success($model->get());
+
+        } catch (\Exception $ex) {
+            return parent::error($ex->getMessage());
+        }
+    }
+     public function getaboutus(Request $request) {
+
+
+        $rules = [];
+        $validateAttributes = parent::validateAttributes($request, 'GET', $rules, array_keys($rules), false);
+        if ($validateAttributes):
+            return $validateAttributes;
+        endif;
+        // dd($category_id);
+        try {
+            $model = new MyModel();
+            $model = $model->select('about_us');
+            
+            return parent::success($model->get());
+
+        } catch (\Exception $ex) {
+            return parent::error($ex->getMessage());
+        }
+    }
     
 }
 
