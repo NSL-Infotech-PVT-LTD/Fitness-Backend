7ad207714bb99b98908c0c20270e4c587324f8c8
niti prabha <niti.prabha@netscapelabs.com>
2019-12-26 13:42:15 +0530

    message to push

--- app/Http/Controllers/API/ConfigurationController.php
+++ app/Http/Controllers/API/ConfigurationController.php
@@ -13,6 +13,37 @@ use Auth;
 
 class ConfigurationController extends ApiController {
 
+    public function getConfigurationColumn(Request $request, $column) {
+        $user = User::findOrFail(\Auth::id());
+        $rules = [];
+        $validateAttributes = parent::validateAttributes($request, 'GET', $rules, array_keys($rules), false);
+        if ($validateAttributes):
+            return $validateAttributes;
+        endif;
+        try {
+
+            if (!in_array($column, ['about_us', 'terms_and_conditions']))
+                return parent::error('Please use valid column');
+//dd($column);
+            $key = '';
+            if ($column == 'terms_and_conditions'):
+                if ($user->hasRole('organizer') === true)
+                    $key = '_organiser';
+                if ($user->hasRole('coach') === true)
+                    $key = '_coach';
+                if ($user->hasRole('athlete') === true)
+                    $key = '_athlete';
+            endif;
+            $model = new MyModel();
+            $model = $model->first();
+            $var = $column . $key;
+//            dd($var);
+            return parent::success($model->$var, 200, 'data');
+        } catch (\Exception $ex) {
+            return parent::error($ex->getMessage());
+        }
+    }
+
     public function getaboutus(Request $request) {
 
 
@@ -24,9 +55,9 @@ class ConfigurationController extends ApiController {
         // dd($category_id);
         try {
             $model = new MyModel();
-            $model = $model->select('about_us');
+            $model = $model->select('about_us')->first();
 
-            return parent::success($model->get());
+            return parent::success($model);
         } catch (\Exception $ex) {
             return parent::error($ex->getMessage());
         }
--- routes/api.php
+++ routes/api.php
@@ -67,9 +67,10 @@ Route::group(['middleware' => 'auth:api'], function() {
     Route::get('terms', 'API\ConfigurationController@getTerms');
     Route::get('logout', 'API\AuthController@logout');
     Route::get('notify/toggle', 'API\AuthController@toggleNotifyUser');
-    Route::post('availability','API\BookingController@getavailability');
+    Route::post('availability', 'API\BookingController@getavailability');
 
 
+    Route::get('configuration/{column}', 'API\ConfigurationController@getConfigurationColumn');
 
 
 
@@ -84,8 +85,6 @@ Route::post('login', 'API\AuthController@Login');
 Route::post('services', 'API\ServicesController@getitems');
 Route::post('sports', 'API\SportController@getitems');
 Route::get('about/us', 'API\ConfigurationController@getaboutus');
-Route::get('terms/coach', 'API\ConfigurationController@getTermsCoach');
-Route::get('terms/athlete', 'API\ConfigurationController@getTermsAthlete');
 
 
 
