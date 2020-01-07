f1b3ca78d658f40b9fa5088a4cbb8e72035e722e
niti prabha <niti.prabha@netscapelabs.com>
2020-01-07 11:06:19 +0530

    message to push

--- app/Http/Controllers/API/ContactController.php
+++ app/Http/Controllers/API/ContactController.php
@@ -13,7 +13,7 @@ use Auth;
 class ContactController extends ApiController {
 
     public function store(Request $request) {
-        $rules = ['message' => 'required', 'media' => 'required'];
+        $rules = ['message' => 'required', 'media' => ''];
         $validateAttributes = parent::validateAttributes($request, 'POST', $rules, array_keys($rules), false);
         if ($validateAttributes):
             return $validateAttributes;
@@ -21,6 +21,7 @@ class ContactController extends ApiController {
         try {
             $input = $request->all();
             $input['created_by'] = \Auth::id();
+            if (isset($request->media))
             $input['media'] = parent::__uploadImage($request->file('media'), public_path('uploads/contact'), true);
             $contact = MyModel::create($input);
             return parent::successCreated(['message' => 'Submitted Successfully', 'contact' => $contact]);
