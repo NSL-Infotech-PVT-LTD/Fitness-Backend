30eeaf29d76d854d0ee1ca1720c7fcb6ea9c0794
Niti <niti.prabha@netscapelabs.com>
2020-02-28 20:26:34 +0530

    migrate

--- app/CoachBooking.php
+++ app/CoachBooking.php
@@ -14,7 +14,7 @@ class CoachBooking extends Model {
     }
 
     public function userDetails() {
-        return $this->hasOne(User::class, 'id', 'user_id')->select('name', 'email', 'phone',
+        return $this->hasOne(User::class, 'id', 'coach_id')->select('name', 'email', 'phone',
                         'address', 'profile_image', 'location', 'business_hour_starts', 'business_hour_ends', 'bio',
                         'expertise_years', 'hourly_rate', 'portfolio_image_1', 'portfolio_image_2', 'portfolio_image_3', 'portfolio_image_4', 'latitude', 'longitude', 'id');
     }
@@ -23,33 +23,10 @@ class CoachBooking extends Model {
         return $this->hasOne(Event::class, 'created_by', 'coach_id')->select('id', 'name', 'description', 'start_date', 'end_date', 'start_time', 'end_time', 'price', 'images_1', 'images_2', 'images_3', 'images_4', 'images_5', 'location', 'latitude', 'longitude', 'service_id', 'created_by', 'guest_allowed', 'equipment_required', 'guest_allowed_left', 'sport_id');
     }
 
-    protected $appends = array('booking_date','images');
+   
 
-    public function getBookingDateAttribute() {
+   
 
-        switch ($this->type):
-
-            case 'coach':
-                $targetModel = new \App\CoachBookingDetail();
-                $model = $targetModel->where('booking_id', $this->id)->get();
-//                dd($this->id);
-//                dd($model->first()->booking_date);
-                if ($model->isEmpty() !== true)
-                    return ['start' => $model->first()->booking_date, 'end' => $model->last()->booking_date];
-
-        endswitch;
-        return [];
-    }
-
-    public function getImagesAttribute() {
-        $model = Event::where('id', $this->created_by)->get();
-        $images = [];
-        for ($i = 1; $i <= 5; $i++):
-            $var = 'images_' . $i;
-            if (isset($model->$var))
-                $images[] = $model->$var;
-        endfor;
-        return json_encode($images);
-    }
+   
 
 }
